229ac42dc5e3ba2630080cd6fe2bf20b3b956017
DawidBialek <dd.bialek@gmail.com>
2021-02-21 21:43:56 +0000

    Code cleanup

--- src/main/java/dawidbialek/NovolTask/job/JobRepository.java
+++ src/main/java/dawidbialek/NovolTask/job/JobRepository.java
@@ -1,13 +1,7 @@
 package dawidbialek.NovolTask.job;
 
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-import java.util.List;
 
 public interface JobRepository extends JpaRepository<Job, Long>{
 
-//    List<Job> findPaged(int pageNumber, int pageSize);
 }
